93da7fbc7d83b8fb9a8a258f85e7a51dceaf0e55
jupahego <jupahego@hotmail.com>
2021-06-22 13:38:01 -0500

    Todos los runners y stepdefinitions

---
+++ Automatizaciones/AutomatizacionBaseScreenplay/AutomatizacionGradle/src/test/java/co/com/qvision/certificacion/demoqa/stepdefinitions/FramesStepDefinitions.java
@@ -0,0 +1,22 @@
+package co.com.qvision.certificacion.demoqa.stepdefinitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class FramesStepDefinitions {
+    @Dado("que estoy en el modulo alerts verifico el item Frames")
+    public void queEstoyEnElModuloAlertsVerificoElItemFrames() {
+
+    }
+
+    @Cuando("verifique y  mueva la barra de iFrame")
+    public void verifiqueYMuevaLaBarraDeIFrame() {
+
+    }
+
+    @Entonces("deberia mostrar la frase This is a sample page")
+    public void deberiaMostrarLaFraseThisIsASamplePage() {
+
+    }
+}
--- Automatizaciones/AutomatizacionBaseScreenplay/AutomatizacionGradle/src/test/resources/features/windows/frames.feature
+++ Automatizaciones/AutomatizacionBaseScreenplay/AutomatizacionGradle/src/test/resources/features/windows/frames.feature
@@ -1,11 +1,10 @@
 #language: es
 
-
-  #Item Frames Scroll
-Característica: Como usuario deseo observar y verificar el item "Frames" del elemento Alerts, Frames & Windows
+Característica: Como usuario deseo observar y verificar el item Frames del elemento Alerts
+  Frames Windows
 
   Escenario: Verificar item Frames
 
     Dado que estoy en el modulo alerts verifico el item Frames
     Cuando verifique y  mueva la barra de iFrame
-    Entonces deberia mostrar la frase "This is a sample page"
+    Entonces deberia mostrar la frase This is a sample page

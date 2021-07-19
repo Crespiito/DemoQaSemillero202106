cd5b806b4a4196a4301487aa41b6c6821e0365bb
Eliana <eliana1128@hotmail.com>
2021-07-19 08:38:32 -0500

    Correccion error en Runner Practice form

--- Automatizaciones/AutomatizacionPOM/AutomatizacionPomMaven/src/test/java/co/com/certificacion/qvision/runners/forms/PracticeFormRunner.java
+++ Automatizaciones/AutomatizacionPOM/AutomatizacionPomMaven/src/test/java/co/com/certificacion/qvision/runners/forms/PracticeFormRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features/forms/practiceForm.feature",
-       // glue = "co.com.certificacion.qvision.stepdefinitions",
-        glue ={"co.com.certificacion.qvision.stepdefinitions.services","co.com.certificacion.qvision.stepdefinitions.hooks"},
+        glue = "co.com.certificacion.qvision.stepdefinitions",
+        //glue ={"co.com.certificacion.qvision.stepdefinitions.forms","co.com.certificacion.qvision.stepdefinitions.hooks"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
 
 )

3d337288aa0d3901f70e2ca1ada9633a0810e1d0
Eliana <eliana1128@hotmail.com>
2021-07-29 17:02:41 -0500

    Ejercicio práctico No 4: Appium + ScreenPlay + Cucumber BDD + Serenity BDD + Gradle/  Ejercicio practico Eribank con ScreenPlay

--- Appium/AppiumWithScreenPlay/src/main/java/co/com/qvision/eribank/userinterfaces/LoginPage.java
+++ Appium/AppiumWithScreenPlay/src/main/java/co/com/qvision/eribank/userinterfaces/LoginPage.java
@@ -5,8 +5,8 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class LoginPage {
 
-    public static final Target NAME_USER = Target.the("Input text user name").located(By.id(""));
-    public static final Target PASS_USER = Target.the("Input text user password").located(By.id(""));
-    public static final Target BUTTON_LOGIN = Target.the("Boton de login").located(By.id(""));
+    public static final Target NAME_USER = Target.the("Input text user name").located(By.id("com.experitest.ExperiBank"));
+    public static final Target PASS_USER = Target.the("Input text user password").located(By.id("com.experitest.ExperiBank"));
+    public static final Target BUTTON_LOGIN = Target.the("Boton de login").located(By.id("com.experitest.ExperiBank"));
 
 }
